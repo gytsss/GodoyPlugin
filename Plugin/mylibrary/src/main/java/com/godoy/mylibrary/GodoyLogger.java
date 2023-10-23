95b30430e4ef5ae29d57b5681a80694b790f87ad
gytsss <totogodoy@hotmail.com>
2023-10-22 21:24:07 -0300

    Add android alert

--- Plugin/mylibrary/src/main/java/com/godoy/mylibrary/GodoyLogger.java
+++ Plugin/mylibrary/src/main/java/com/godoy/mylibrary/GodoyLogger.java
@@ -1,19 +1,128 @@
 package com.godoy.mylibrary;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class GodoyLogger {
 
     static final String LOGTAG = "GodLOG";
+    public static Activity mainActivity;
+
+    public interface AlertViewCallBack {
+        public void onButtonTapped(int id);
+    }
 
     static GodoyLogger _instance = null;
-    public static GodoyLogger getInstance()
-    {
-        if(_instance == null)
+
+    public static GodoyLogger getInstance() {
+        if (_instance == null)
             _instance = new GodoyLogger();
         return _instance;
     }
 
-    public String getLogtag()
-    {
+    public String getLogtag() {
         return LOGTAG;
     }
+
+    public void showAlertView(String[] strings, final AlertViewCallBack callBack) {
+        if (strings.length < 3) {
+            Log.i(LOGTAG, "Error - expected at least 3 strings, got " + strings.length);
+            return;
+        }
+
+        DialogInterface.OnClickListener myClickListener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                Log.i(LOGTAG, "Tapped: " + which);
+                callBack.onButtonTapped(which);
+            }
+        };
+
+        AlertDialog alertDialog = new AlertDialog.Builder(mainActivity)
+                .setTitle(strings[0])
+                .setMessage(strings[1])
+                .setCancelable(false)
+                .create();
+        alertDialog.setButton(alertDialog.BUTTON_NEUTRAL, strings[2], myClickListener);
+        if (strings.length > 3)
+            alertDialog.setButton(alertDialog.BUTTON_NEGATIVE, strings[3], myClickListener);
+        if (strings.length > 4)
+            alertDialog.setButton(alertDialog.BUTTON_POSITIVE, strings[4], myClickListener);
+        alertDialog.show();
+
+    }
+    public void showAlertBeforeClearingLogs()
+    {
+        String[] strings = {"Confirmation", "Are you sure that you want to delete logs?", "Yes", "No"};
+
+        showAlertView(strings, new AlertViewCallBack() {
+            @Override
+            public void onButtonTapped(int id) {
+                if(id == DialogInterface.BUTTON_NEUTRAL)
+                {
+                    clearUnityLogs();
+                }
+            }
+        });
+    }
+
+    private ArrayList<String> unityLogs = new ArrayList<>();
+    private File logFile;
+
+    public GodoyLogger() {
+        logFile = new File(mainActivity.getFilesDir(), "unity_logs.txt");
+
+        if (!logFile.exists()) {
+            try {
+                logFile.createNewFile();
+            } catch (IOException e) {
+                Log.e(LOGTAG, "Error creating logs file", e);
+            }
+        }
+    }
+
+    public void logFromUnity(String log) {
+        unityLogs.add(log);
+
+        try {
+            FileOutputStream fos = new FileOutputStream(logFile, true);
+            fos.write((log + "\n").getBytes());
+            fos.close();
+        } catch (IOException e) {
+            Log.e(LOGTAG, "Error writing logs file", e);
+        }
+    }
+
+    public ArrayList<String> getUnityLogs()
+    {
+        return unityLogs;
+    }
+
+    public void clearUnityLogs()
+    {
+        unityLogs.clear();
+        if(logFile.exists())
+        {
+            logFile.delete();
+            try
+            {
+                logFile.createNewFile();
+            }
+            catch (IOException e)
+            {
+                Log.e(LOGTAG, "Error deleting logs file", e);
+            }
+        }
+    }
+
+
+
 }
--- Unity/PluginGodoy/Assets/TestPlugin.cs
+++ Unity/PluginGodoy/Assets/TestPlugin.cs
@@ -8,9 +8,25 @@ public class TestPlugin : MonoBehaviour
 {
     private const string packageName = "com.godoy.mylibrary";
     const string className = packageName + ".GodoyLogger";
-    
-    #if UNITY_ANDROID 
-    
+
+
+    class AlertViewCallBack : AndroidJavaProxy
+    {
+        private System.Action<int> alertHandler;
+
+        public AlertViewCallBack(System.Action<int> alertHandlerIn) : base(className + "$AlertViewCallBack")
+        {
+            alertHandler = alertHandlerIn;
+        }
+
+        public void onButtonTapped(int index)
+        {
+            Debug.Log("Button tapped: " + index);
+            if (alertHandler != null)
+                alertHandler(index);
+        }
+    }
+
     AndroidJavaClass _pluginClass;
     AndroidJavaObject _pluginInstance;
 
@@ -21,19 +37,57 @@ public class TestPlugin : MonoBehaviour
         if (Application.platform == RuntimePlatform.Android)
         {
             _pluginClass = new AndroidJavaClass(className);
+            
+            AndroidJavaClass playerClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+            AndroidJavaObject activity = playerClass.GetStatic<AndroidJavaObject>("currentActivity");
+            _pluginClass.SetStatic<AndroidJavaObject>("mainActivity", activity);
+            
             _pluginInstance = _pluginClass.CallStatic<AndroidJavaObject>("getInstance");
+            
+            // _pluginInstance.Call("logFromUnity", "Este es un log desde Unity");
+            // ArrayList logs = _pluginInstance.Call<ArrayList>("getUnityLogs");
+            // _pluginInstance.Call("showAlertBeforeClearingLogs");
+
+
         }
     }
 
     public void RunPlugin()
     {
         Debug.Log("RunPlugin()");
-        
+
         if (Application.platform == RuntimePlatform.Android)
         {
             label.text = _pluginInstance.Call<string>("getLogtag");
+            _pluginInstance.Call("logFromUnity", "Este es un log desde C#");
+        }
+    }
+
+    void showAlertDialog(string[] strings, System.Action<int> handler = null)
+    {
+        if (strings.Length < 3)
+        {
+            Debug.LogError("AlertView requieres at least 3 strings");
+            return;
         }
 
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            _pluginInstance.Call("showAlertView", new object[] { strings, new AlertViewCallBack(handler) });
+            
+        }
+        else
+            Debug.Log("AlertView not supported on this platform");
     }
-    #endif  
+
+    public void ShowAlert()
+    {
+        Debug.Log("ShowAlert()");
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            label.text = "POP UP PLEASE";
+            showAlertDialog(new string[] { "Alert Title", "Alert Message","Cancel" , "OK" }, (int obj) => { Debug.Log("Local Handler called: " + obj); });
+            //_pluginInstance.Call("showAlertBeforeClearingLogs");
+        }
+    }
 }
